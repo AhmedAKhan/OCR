18b8d58cee33c65e81ccc08ba55ee6b7c0a31ba7
AhmedAKhan <khanaahmed10@gmail.com>
2015-12-24 18:22:59 -0500

    made the files using the yeomen generator

---
+++ handwritingWebappYaomen/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name handwritingWebappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the handwritingWebappApp
+ */
+angular.module('handwritingWebappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ handwritingWebappYaomen/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name handwritingWebappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the handwritingWebappApp
+ */
+angular.module('handwritingWebappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
